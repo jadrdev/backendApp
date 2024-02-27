58acaa87554ebfe4f8a067424b30a62ff5cfff56
JadrDevSAO <jdiarob@saosl.com>
2024-02-27 16:13:01 +0000

    Agregadas dependencias y configuración de TypeORM y MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
